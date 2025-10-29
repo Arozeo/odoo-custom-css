27cb50f15fab13e780bb318bee6145e2d19a8ad2
Eliott@Arozeo <contact@tadezami.app>
2025-10-29 13:24:14 +0100

    Ajout de try-catch pour fail-safe

--- v17/static/src/js/dynamic_css.js
+++ v17/static/src/js/dynamic_css.js
@@ -1,14 +1,32 @@
 odoo.define('v17.dynamic_css', [], function (require) {
     'use strict';
 
-    var cssUrl = 'https://raw.githubusercontent.com/Arozeo/odoo-custom-css/refs/heads/master/v17/static/src/css/arozeo_custom.css';
+    try {
+        var cssUrl = 'https://raw.githubusercontent.com/Arozeo/odoo-custom-css/refs/heads/master/v17/static/src/css/arozeo_custom.css';
 
-    var link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = cssUrl;
-    link.type = 'text/css';
-
-    document.head.appendChild(link);
-
-    console.log('CSS loaded from:', cssUrl);
+        // Fetch the CSS content
+        fetch(cssUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.text();
+            })
+            .then(cssText => {
+                console.log('CSS content loaded, length:', cssText.length);
+                // Create a style element
+                var style = document.createElement('style');
+                style.type = 'text/css';
+                style.textContent = cssText;
+                // Add to head
+                document.head.appendChild(style);
+                console.log('CSS style added to head');
+            })
+            .catch(error => {
+                console.error('Error loading CSS:', error);
+            });
+    } catch (error) {
+        console.error('Error in dynamic CSS module:', error);
+        // Do nothing on error
+    }
 });
